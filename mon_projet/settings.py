10b62410895427ec19d44ed6a018767c8d4a8e71
Anasse0 <145682783+Anasse0@users.noreply.github.com>
2026-04-10 00:40:29 +0200

    Clean up settings.py by removing comments
    Removed commented-out settings for production configuration.

--- mon_projet/settings.py
+++ mon_projet/settings.py
@@ -17,18 +17,6 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-^j&&2f96&4tqz@*=-s3lr&e&g3g0uz2$573i!)+%%j!685n=lt'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-
-#ALLOWED_HOSTS = []
-
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
@@ -78,17 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mon_projet.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL'))
@@ -146,11 +123,6 @@ REST_FRAMEWORK = {
     ],
     'EXCEPTION_HANDLER': 'mon_app.exception_handler.custom_exception_handler',
 }
-
-#CORS_ALLOWED_ORIGINS = [
-#    'http://localhost:3000',   # Create React App
-#    'http://localhost:5173',   # Vite
-#]
 
 from datetime import timedelta
 SIMPLE_JWT = {
