8aa51d45919f71faa2f032fad4d2535c3cca60e6
Anasse0 <145682783+Anasse0@users.noreply.github.com>
2026-04-09 23:56:24 +0200

    Update start.sh to exclude superuser creation
    Remove superuser creation from start script.

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 python manage.py migrate --noinput
-if [ "$DJANGO_SUPERUSER_USERNAME"]; then 
-    python manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL
-fi
 gunicorn mon_projet.wsgi:application --bind 0.0.0.0:$PORT
